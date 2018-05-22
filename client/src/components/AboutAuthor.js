5c0734c10b4d40a0e9eae3db93cbc8dca13eeb20
Phil S <philipstubbs13@gmail.com>
2018-05-22 07:35:24 -0500

    updated color scheme

--- client/src/components/AboutAuthor.js
+++ client/src/components/AboutAuthor.js
@@ -1,24 +1,29 @@
 // Importing React since we are using React.
 import React, { Component } from 'react';
 // Importing UI components from rebass.
-import { Box, Heading, Text } from 'rebass';
+import { Box, Heading, Text, Panel, Header } from 'rebass';
 
 
 class AboutAuthor extends Component {
   render() {
     return [
-      <Box mt={4}>
-        <Heading
-          is='h3'
-         children="About the author"
-        />
-        <Text fontSize={18} pt={2}>
-          In 2017, I decided to chase after a dream and learn how to code. Ever since then, my life has been turned upside down. For me, learning how to code taught me how to think critically and solve problems creatively and quickly. There's nothing more satisfying than staying up all night to figure out a bug in your code and figure out a solution at 3:30 in the morning (hence, the name of this blog). 
-        </Text>
-        <Text fontSize={18} pt={2}>
-        With learning how to code comes frustration, energy drinks, and many late night coding sessions. But, in my opinion, the challenges are what make coding so exciting and wanting me coming back for more. There are always new tools and technologies to learn. There's never a dull moment in learning how to code or trying to figure out a solution to a complex problem. I started this website to share my passion. I hope my experiences in learning how to code will help and inspire you on your coding journey.
-        </Text>
-      </Box>
+      <Panel color='black' className="blogPostCard">
+        <Panel.Header
+          color='white'
+          bg='#001F54'
+          fontSize={28}
+          px={4}>
+          About the author
+        </Panel.Header>
+        <Box mx={4} my={2}>
+          <Text fontSize={18} pt={2}>
+            In 2017, I decided to chase after a dream and learn how to code. Ever since then, my life has been turned upside down. For me, learning how to code taught me how to think critically and solve problems creatively and quickly. There's nothing more satisfying than staying up all night to figure out a bug in your code and figure out a solution at 3:30 in the morning (hence, the name of this blog). 
+          </Text>
+          <Text fontSize={18} pt={2}>
+          With learning how to code comes frustration, energy drinks, and many late night coding sessions. But, in my opinion, the challenges are what make coding so exciting and wanting me coming back for more. There are always new tools and technologies to learn. There's never a dull moment in learning how to code or trying to figure out a solution to a complex problem. I started this website to share my passion. I hope my experiences in learning how to code will help and inspire you on your coding journey.
+          </Text>
+        </Box>
+      </Panel>
     ];
   }
 }
--- client/src/components/AboutBlog.js
+++ client/src/components/AboutBlog.js
@@ -1,17 +1,21 @@
 // Importing React since we are using React.
 import React, { Component } from 'react';
 // Importing UI components from rebass.
-import { Box, Heading, Text } from 'rebass';
+import { Box, Heading, Text, Panel, Header } from 'rebass';
 
 
 class AboutBlog extends Component {
   render() {
     return [
-      <Box mt={4}>
-        <Heading
-          is='h3'
-         children="About this blog"
-        />
+      <Panel color='black' className="blogPostCard" mt={4}>
+        <Panel.Header
+          color='white'
+          bg='#001F54'
+          fontSize={28}
+          px={4}>
+          About this blog
+        </Panel.Header>
+        <Box mx={4} my={2}>
         <Text fontSize={18} pt={2}>
           I created this blog to combine two of my passions, coding and writing. Specifically, I started this blog to jot down some of my thoughts and ideas about the many challenges of learning how to code and about the interesting, late night adventures of being a full stack web developer. 
         </Text>
@@ -19,6 +23,7 @@ class AboutBlog extends Component {
         My goal in writing this blog is to engage and build a community of developers in the Twin Cities by highlighting coding events in the Twin Cities area, sharing my experiences of learning how to code from the University of Minnesota full stack web development program, writing reviews about some of the courses I have taken, and going through some of the projects I am currently working on in my free time.
         </Text>
       </Box>
+    </Panel>
     ];
   }
 }
